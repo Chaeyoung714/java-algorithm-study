c061bc3883fd1e124d3930605fab95385b1e4631
Chaeyoung714 <202102760@hufs.ac.kr>
2025-07-31 11:07:41 +0900

    docs: 알고리즘 템플릿 정리

--- note/Template.java
+++ note/Template.java
@@ -1,10 +1,25 @@
 package org.example.note;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Template {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        bw.write(str); // Str으로 변환해서 출력해야 한다.
+        bw.flush();
+
 
     }
 }
