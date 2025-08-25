adb7d9e54fe5b96197424bc35e9b794f30fb6a5e
Chaeyoung714 <202102760@hufs.ac.kr>
2025-08-25 23:55:43 +0900

    solving: numberSort 10989

---
+++ doItAlgorithm/정렬/NumberSort10989.java
@@ -0,0 +1,29 @@
+package org.example.doItAlgorithm.정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+[[]]
+public class NumberSort10989 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(numbers);
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : numbers) {
+            sb.append(num + "\n");
+        }
+
+        System.out.println(sb);
+    }
+}
